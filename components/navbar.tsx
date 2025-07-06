e2eaa86f0c522cad2374514572423a0d0ab57dfc
Audora <gy2553@naver.com>
2025-07-06 16:05:56 +0900

    헤더부분 순서 변경

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -35,21 +35,14 @@ export function Navbar() {
           </Link>
 
           <div className="flex items-center space-x-4">
-            {/* 대시보드 버튼 */}
-            <Link href="/dashboard">
-              <Button variant="ghost" className="text-white hover:bg-white/20">
-                <Home className="mr-2 h-4 w-4" />
-                대시보드
-              </Button>
-            </Link>
+            {/* 닉네임 표시: 클라이언트 마운트 후 렌더링 */}
+            {user && <span className="text-white font-medium">{user}</span>}
+            {/* 닉네임 변경 모달 트리거 */}
             {/* 알림 아이콘 */}
             <Button variant="ghost" size="icon" className="relative text-white hover:bg-white/20">
               <Bell className="h-5 w-5" />
               <span className="absolute top-1 right-1 h-2 w-2 rounded-full bg-red-500" />
             </Button>
-            {/* 닉네임 표시: 클라이언트 마운트 후 렌더링 */}
-            {user && <span className="text-white font-medium">{user}</span>}
-            {/* 닉네임 변경 모달 트리거 */}
             <ChangeNicknameModal />
             {/* 로그아웃 */}
             <Button onClick={handleLogout} variant="ghost" className="text-white hover:bg-white/20">
